ddd380f8c03359bc2bbdaaf8fa57e34d7dd45b85
shoarora <shoarora@stanford.edu>
2019-02-16 13:11:30 -0800

    Clean up manifest, make http requests from background

--- client/src/background.js
+++ client/src/background.js
@@ -1,22 +1,20 @@
-chrome.runtime.onMessage.addListener(function(request, sender, callback) {
-    if (request.action == "xhttp") {
-        var xhttp = new XMLHttpRequest();
-        var method = request.method ? request.method.toUpperCase() : 'POST';
+chrome.runtime.onInstalled.addListener(function() {});  // Triggers adding other listeners
 
-        xhttp.onload = function() {
-            console.log(xhttp.responseText);
-            callback(xhttp.responseText);
-        };
-        xhttp.onerror = function() {
-            // Do whatever you want on error. Don't forget to invoke the
-            // callback to clean up the communication port.
-            callback();
-        };
-        xhttp.open(method, request.url, true);
-        if (method == 'POST') {
-            xhttp.setRequestHeader('Content-Type', 'application/json');
-        }
-        xhttp.send(request.data);
-        return true; // prevents the callback from being called too early on return
-    }
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+  if (request.action == "xhttp") {
+    $.ajax({
+      type: request.method,
+      url: request.url,
+      data: request.data,
+      contentType: 'application/json',
+      success: function(responseText) {
+        callback(responseText, 'success');
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        //if required, do some error handling
+        callback(textStatus, 'failed');
+      }
+    });
+  }
+  return true;
 });
--- client/src/content.js
+++ client/src/content.js
@@ -1,83 +1,22 @@
-/* File: content.js
- * ---------------
- * Hello! You'll be making most of your changes
- * in this file. At a high level, this code replaces
- * the substring "cal" with the string "butt" on web pages.
- *
- * This file contains javascript code that is executed
- * everytime a webpage loads over HTTP or HTTPS.
- */
+$(window).on('load', function() {
+  var currentURL = {
+    url: window.location.href
+  };
 
-if(document.readyState === 'interactive') {
-    // good to go!
-    console.log("starting extension...");
-    var currentURL = {url:window.location.href};
-    console.log(currentURL);
-
-    /*
-    var xhr = new XMLHttpRequest();
-    //xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
-    xhr.open("POST", chrome.extension.getURL('/config_resources/config.json'), true);
-    xhr.send();
-    */
-
-    /*
-    $.ajax({
-        type: "POST",
-        url: "http://104.40.74.37:5000/create_music/",
-        success: playFile,
-        contentType: "application/json",
-        data: JSON.stringify(currentURL)
-    });
-    */
-    chrome.runtime.sendMessage({
-        method: 'POST',
-        action: 'xhttp',
-        url: 'http://104.40.74.37:5000/create_music/',
-        data: JSON.stringify(currentURL)
-    }, function(audio) {
-        playAudio(audio);
-        /*Callback function to deal with the response*/
-    });
-
-
-    //$.post("https://104.40.74.37:5000/create_music/", JSON.stringify(currentURL), playFile);
-    //console.log(status);
-
-}
+  chrome.runtime.sendMessage({
+    method: 'POST',
+    action: 'xhttp',
+    url: 'http://104.40.74.37:5000/create_music/',
+    data: JSON.stringify(currentURL)
+  }, function(audio, status) {
+    playAudio(audio, status);
+  });
+});
 
 function playAudio(filename, status, jqxhr_object) {
-    console.log(status);
-    console.log(filename);
-    var audio = new Audio("http://104.40.74.37:5000/wavs/" + filename);
-    audio.load();
-    audio.play();
+  console.log(status);
+  console.log(filename);
+  var audio = new Audio("http://104.40.74.37:5000/wavs/" + filename);
+  audio.load();
+  audio.play();
 }
-/*
-$("button").click(function(){
-        $.post("http://104.40.74.37:5000/create_music/", currentURL, playFile(data, status));
-    });
-*/
-//TODO
-//finish post command
-//figure out audio
-
-
-/*
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/cal/gi, "butt"); // replaces "cal," "Cal", etc. with "butt"
-
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-              }
-        }
-    }
-}
-*/
