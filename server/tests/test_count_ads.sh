09e31f92c3dd7ae89ea58667f8469c325a018347
Michael Svolos <michael.svolos@gmail.com>
2019-02-16 11:45:44 -0800

    added test cases

--- server/tests/test_count_ads.sh
+++ server/tests/test_count_ads.sh
@@ -2,10 +2,66 @@ curl http://104.40.74.37:5000/count_ads/ -X POST \
   -H "Content-Type: application/json" \
   -d '{"url":"https://youtube.com"}'
 
-echo "\n"
+echo "\n1\n"
 
 curl http://104.40.74.37:5000/count_ads/ -X POST \
   -H "Content-Type: application/json" \
   -d '{"url":"https://www.cnet.com/"}'
 
+echo "\n2\n"
+
+curl http://104.40.74.37:5000/count_ads/ -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"url":"https://www.wowt.com/"}'
+
+echo "\n3\n"
+
+curl http://104.40.74.37:5000/count_ads/ -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"url":"http://www.coatesmeister.org/"}'
+
+echo "\n4\n"
+
+curl http://104.40.74.37:5000/count_ads/ -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"url":"https://www.facebook.com/"}'
+
+echo "\n5\n"
+
+curl http://104.40.74.37:5000/count_ads/ -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"url":"https://www.twitch.tv"}'
+
+echo "\n6\n"
+
+ curl http://104.40.74.37:5000/count_ads/ -X POST \
+   -H "Content-Type: application/json" \
+   -d '{"url":"https://www.reddit.com"}'
+
 echo "\n"
+
+
+curl http://104.40.74.37:5000/count_ads/ -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"url":"https://www.questionablecontent.net/"}'
+
+echo "\n7\n"
+
+
+curl http://104.40.74.37:5000/count_ads/ -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"url":"http://www.youtube-mp3.org/"}'
+
+echo "\n8\n"
+
+curl http://104.40.74.37:5000/count_ads/ -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"url":"http://www.logicalincrements.com/"}'
+
+echo "\n9\n"
+
+curl http://104.40.74.37:5000/count_ads/ -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"url":"https://cocland.com/"}'
+
+echo "\n10\n"
