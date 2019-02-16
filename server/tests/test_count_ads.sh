2eb8f941d79c45f6a16e6e7f8fb2eb74a67d30af
shoarora <shoarora@stanford.edu>
2019-02-16 00:31:05 -0800

    Fix tests

--- server/tests/test_count_ads.sh
+++ server/tests/test_count_ads.sh
@@ -2,6 +2,10 @@ curl http://104.40.74.37:5000/count_ads/ -X POST \
   -H "Content-Type: application/json" \
   -d '{"url":"https://youtube.com"}'
 
+echo "\n"
+
 curl http://104.40.74.37:5000/count_ads/ -X POST \
   -H "Content-Type: application/json" \
   -d '{"url":"https://www.cnet.com/"}'
+
+echo "\n"
--- server/tests/test_create_music.sh
+++ server/tests/test_create_music.sh
@@ -1,3 +1,3 @@
 curl http://104.40.74.37:5000/create_music/ -X POST \
   -H "Content-Type: application/json" \
-  -d '{"count":"42"}'
+  -d '{"count":"42"}' > bar.wav
